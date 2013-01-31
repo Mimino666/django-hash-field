f5b4ace53820ae97719570f6ce7332e70b386180
Michal Danilak <mimino.coder@gmail.com>
2013-01-31 01:35:17 +0100

    Renamed from HashedField to HashField.

--- hashfield.py
+++ hashfield.py
@@ -8,8 +8,8 @@ from south.modelsinspector import add_introspection_rules
 _hashit = lambda s: hashlib.sha1(s).hexdigest()
 
 
-class HashedField(models.CharField):
-    description = ('HashedField is related to some other field in a model and'
+class HashField(models.CharField):
+    description = ('HashField is related to some other field in a model and'
         'stores its hashed value for better indexing performance.')
 
     def __init__(self, original, *args, **kwargs):
@@ -23,7 +23,7 @@ class HashedField(models.CharField):
         kwargs['db_index'] = True
         kwargs['null'] = False
         kwargs['editable'] = False
-        super(HashedField, self).__init__(*args, **kwargs)
+        super(HashField, self).__init__(*args, **kwargs)
 
     def calculate_hash(self, model_instance):
         original_value = getattr(model_instance, self.original)
@@ -31,18 +31,18 @@ class HashedField(models.CharField):
 
     def pre_save(self, model_instance, add):
         self.calculate_hash(model_instance)
-        return super(HashedField, self).pre_save(model_instance, add)
+        return super(HashField, self).pre_save(model_instance, add)
 
 
 # This tell South, how to store your field.
 # more information: http://south.readthedocs.org/en/0.7.6/customfields.html
 add_introspection_rules([
     (
-        [HashedField],
+        [HashField],
         [],
         {
             "original": ["original", {}],
         },
     ),
 ],
-["^myproject\.fields\.HashedField"])  # use your own path to HashedField
+["^myproject\.fields\.HashField"])  # use your own path to HashField
